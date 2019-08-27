54a160f5b4dbe4b91acead00cee6e63bd416c2ca
fx <1589271372@qq.com>
2019-08-27 16:27:20 +0800

    v1.1

--- src/main/java/com/baizhi/EchartsApplication.java
+++ src/main/java/com/baizhi/EchartsApplication.java
@@ -12,6 +12,7 @@ public class EchartsApplication {
         System.out.println("666");
         System.out.println("你好，我是练习时长两年半的偶像练习生");
         System.out.println("你会唱跳rap篮球吗？");
+        System.out.println("导播，走曲儿");
     }
 
 }
