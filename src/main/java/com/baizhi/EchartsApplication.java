180b036ab7dd6d0d8ab55b84a5942af665db3884
fx <1589271372@qq.com>
2019-08-27 15:22:41 +0800

    v1.0

--- src/main/java/com/baizhi/EchartsApplication.java
+++ src/main/java/com/baizhi/EchartsApplication.java
@@ -9,6 +9,7 @@ public class EchartsApplication {
     public static void main(String[] args) {
         SpringApplication.run(EchartsApplication.class, args);
         System.out.println();
+        System.out.println("666");
     }
 
 }
